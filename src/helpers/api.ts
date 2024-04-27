3f38bdf51a5e4c82d85094582fd3016c80747e8e
harryob <55142896+harryob@users.noreply.github.com>
2024-04-27 09:59:22 +0100

    only if we don't already have that though

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,7 +1,13 @@
 export const callApi = async (toCall: string, init?: RequestInit | undefined): Promise<Response> => {
   const response = await fetch(`${import.meta.env.VITE_API_PATH}${toCall}`, init)
   if (response.status == 400) {
+    const amount = location.href.search(/\?forceRefresh=true/);
+    if(amount > 0) {
+      location.reload();
+    }
+    else {
     location.replace(location.href + "?forceReload=true")
+    }
   }
   return response
 }
