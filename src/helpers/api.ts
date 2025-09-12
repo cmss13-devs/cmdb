154eec2a6228a777973483322faa582a7b282e7a
harryob <55142896+harryob@users.noreply.github.com>
2025-09-12 18:22:14 +0100

    do it this way instead duh

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,5 +1,5 @@
 export const callApi = async (toCall: string, init?: RequestInit | undefined): Promise<Response> => {
-  const response = await fetch(`${import.meta.env.VITE_API_PATH}${toCall}`, init)
+  const response = await fetch(`${location.protocol}//${location.host}/api/${toCall}`, init)
   if (response.status == 400) {
     location.replace(`${location.protocol}//${location.host}/?existing=${location.hash.substring(1)}`)
   }
