c74a6089865cfe8681cf2f2e4eb13a370eecd68c
harryob <55142896+harryob@users.noreply.github.com>
2025-12-22 14:44:07 +0000

    lintless

--- src/main.tsx
+++ src/main.tsx
@@ -1,64 +1,65 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
-import { LookupMenu } from "./components/userLookup";
+import { AuthentikPanel } from "./components/authentikPanel";
+import HomePage from "./components/homePage";
+import { NewPlayers } from "./components/newPlayers";
 import { Stickybans } from "./components/stickybans";
 import { Tickets } from "./components/tickets";
-import HomePage from "./components/homePage";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { LookupMenu } from "./components/userLookup";
 import { WhitelistMenu } from "./components/whitelistPanel";
-import { NewPlayers } from "./components/newPlayers";
-import { AuthentikPanel } from "./components/authentikPanel";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-		children: [
-			{
-				path: "",
-				element: <HomePage />,
-			},
-			{
-				path: "/ticket/:round?/:ticketNum?",
-				element: <Tickets />,
-				loader: ({ params }) => {
-					return {
-						round: params.round || "",
-						ticketNum: params.ticketNum || "",
-					};
-				},
-			},
-			{
-				path: "/sticky",
-				element: <Stickybans />,
-			},
-			{
-				path: "/user/:ckey?",
-				element: <LookupMenu />,
-				loader: ({ params }) => {
-					return params.ckey || "";
-				},
-			},
-			{
-				path: "/whitelists",
-				element: <WhitelistMenu />,
-			},
-			{
-				path: "/new_players",
-				element: <NewPlayers />,
-			},
-			{
-				path: "/user_manager",
-				element: <AuthentikPanel />,
-			},
-		],
-	},
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "/ticket/:round?/:ticketNum?",
+        element: <Tickets />,
+        loader: ({ params }) => {
+          return {
+            round: params.round || "",
+            ticketNum: params.ticketNum || "",
+          };
+        },
+      },
+      {
+        path: "/sticky",
+        element: <Stickybans />,
+      },
+      {
+        path: "/user/:ckey?",
+        element: <LookupMenu />,
+        loader: ({ params }) => {
+          return params.ckey || "";
+        },
+      },
+      {
+        path: "/whitelists",
+        element: <WhitelistMenu />,
+      },
+      {
+        path: "/new_players",
+        element: <NewPlayers />,
+      },
+      {
+        path: "/user_manager",
+        element: <AuthentikPanel />,
+      },
+    ],
+  },
 ]);
 
+// biome-ignore lint/style/noNonNullAssertion: this has to exist
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
